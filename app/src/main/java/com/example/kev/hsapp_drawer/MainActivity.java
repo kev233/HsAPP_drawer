e23db82796f9dfe80c315b7b1fd05b79cb5ee284
kev233 <44610339+kev233@users.noreply.github.com>
2018-11-22 14:33:03 +0100

    Test Push

--- app/src/main/java/com/example/kev/hsapp_drawer/MainActivity.java
+++ app/src/main/java/com/example/kev/hsapp_drawer/MainActivity.java
@@ -28,7 +28,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
                 R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
-        toggle.syncState(); //rotiert das Menüsymbole
+        toggle.syncState(); //rotiert das Menüsymbolee
 
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
